28c40a0a3002522ba19d820049ac231414efc9ab
Akila Ishan <59306630+AkilaS8@users.noreply.github.com>
2021-09-14 17:50:25 +0530

    add rocket list tile

---
+++ lib/features/feature_rockets/presentation/widgets/rocket_list_tile_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ListTile extends StatefulWidget {
+  const ListTile({Key? key}) : super(key: key);
+
+  @override
+  _ListTileState createState() => _ListTileState();
+}
+
+class _ListTileState extends State<ListTile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
