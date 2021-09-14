5600c8e905584eb409f6a841d9094d8c2b006945
Akila Ishan <59306630+AkilaS8@users.noreply.github.com>
2021-09-14 17:35:40 +0530

    First commit

---
+++ lib/features/feature_rockets/presentation/pages/rockets_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RocketsPage extends StatefulWidget {
+  const RocketsPage({Key? key}) : super(key: key);
+
+  @override
+  _RocketsPageState createState() => _RocketsPageState();
+}
+
+class _RocketsPageState extends State<RocketsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
