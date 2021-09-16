45072315127c496ac2c2c61b7f7a8977a0c62a5b
sunalii <sunaliepic@gmail.com>
2021-09-16 20:15:44 +0530

    missions usecases added

--- lib/features/feature_missions/data/data_source/remote_data_source/missions_remote_data_data_source.dart
+++ lib/features/feature_missions/data/data_source/remote_data_source/missions_remote_data_data_source.dart
@@ -5,11 +5,11 @@ import 'package:space_x/core/error/exception.dart';
 import 'package:space_x/features/feature_missions/data/models/missions_data_model.dart';
 import 'package:http/http.dart' as http;
 
-abstract class MissionRemoteDataSource {
+abstract class MissionsRemoteDataSource {
   Future<List<MissionsDataModel>> getMissionsDetails();
 }
 
-class MissionRemoteDataSourceImpl extends MissionRemoteDataSource {
+class MissionRemoteDataSourceImpl extends MissionsRemoteDataSource {
   final http.Client client;
 
   MissionRemoteDataSourceImpl({required this.client});
