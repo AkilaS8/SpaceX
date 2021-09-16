86e31784c53e97854bfc14d204361cc6a4b1ce54
Akila Ishan <59306630+AkilaS8@users.noreply.github.com>
2021-09-16 09:46:20 +0530

    add dragon page to vehicles navigation

--- lib/features/feature_vehicles/core/vehicles_navigation_list.dart
+++ lib/features/feature_vehicles/core/vehicles_navigation_list.dart
@@ -1,5 +1,7 @@
+import 'package:space_x/features/feature_dragon/presentation/pages/dragons_page.dart';
 import 'package:space_x/features/feature_rockets/presentation/pages/rockets_page.dart';
 
 const kVehiclePages = [
   RocketsPage(),
+  DragonsPage(),
 ];
