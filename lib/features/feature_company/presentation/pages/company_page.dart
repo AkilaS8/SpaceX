6a9b47c73e3c6ccb1a0d960def7c460d40816f18
sunalii <sunaliepic@gmail.com>
2021-09-17 12:54:38 +0530

    company page data and domain layers created

--- lib/features/feature_company/presentation/pages/company_page.dart
+++ lib/features/feature_company/presentation/pages/company_page.dart
@@ -23,45 +23,49 @@ class _CompanyPageState extends State<CompanyPage> {
           title: Text("Company"),
         ),
         drawer: Drawer(),
-        body: Container(
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              children: [
-                Image.asset("assets/images/space_x_2.png"),
-                SizedBox(height: 20.0),
-                Text(
-                  "SpaceX designs, manufactures and launches advanced rockets and spacecrafts. The company was founded in 2002 to revolutionize space technology, with the ultimate goal of enabling people to live on other"
+        body: buildBody(),
+      ),
+    );
+  }
+
+  buildBody() {
+    return Container(
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            Image.asset("assets/images/space_x_2.png"),
+            SizedBox(height: 20.0),
+            Text(
+              "SpaceX designs, manufactures and launches advanced rockets and spacecrafts. The company was founded in 2002 to revolutionize space technology, with the ultimate goal of enabling people to live on other"
                   " planets.",
-                  style: TextStyle(color: Colors.white),
-                ),
-                SizedBox(height: 30.0),
-                Align(
-                  alignment: Alignment.topLeft,
-                  child: Container(
-                    width: 120.0,
-                    child: Row(
-                      children: [
-                        Icon(Icons.location_on, color: Colors.white,),
-                        SizedBox(width: 10.0),
-                        Text("Location", style: TextStyle(color: Colors.white),),
-                      ],
-                    ),
-                  ),
-                ),
-                Column(
+              style: TextStyle(color: Colors.white),
+            ),
+            SizedBox(height: 30.0),
+            Align(
+              alignment: Alignment.topLeft,
+              child: Container(
+                width: 120.0,
+                child: Row(
                   children: [
-                    ListView(
-                      children: [
-                        Text("Location", style: TextStyle(color: Colors.white),), // NOT VISIBLE IN SCREEN
-                        Text("Location", style: TextStyle(color: Colors.white),), // NOT VISIBLE IN SCREEN
-                      ],
-                    ),
+                    Icon(Icons.location_on, color: Colors.white,),
+                    SizedBox(width: 10.0),
+                    Text("Location", style: TextStyle(color: Colors.white),),
+                  ],
+                ),
+              ),
+            ),
+            Column(
+              children: [
+                ListView(
+                  children: [
+                    Text("Location", style: TextStyle(color: Colors.white),), // NOT VISIBLE IN SCREEN
+                    Text("Location", style: TextStyle(color: Colors.white),), // NOT VISIBLE IN SCREEN
                   ],
                 ),
               ],
             ),
-          ),
+          ],
         ),
       ),
     );
