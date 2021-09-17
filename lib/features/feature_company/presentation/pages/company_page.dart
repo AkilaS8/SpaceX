e7dda967fab99fec70a63545af10814b9681e5b4
sunalii <sunaliepic@gmail.com>
2021-09-17 14:39:41 +0530

    company - page created with body widget

--- lib/features/feature_company/presentation/pages/company_page.dart
+++ lib/features/feature_company/presentation/pages/company_page.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:space_x/core/constants/colors.dart';
+import 'package:space_x/features/feature_company/presentation/bloc/company_bloc.dart';
+import 'package:space_x/features/feature_company/presentation/widgets/company_loading.dart';
+import 'package:space_x/features/feature_company/presentation/widgets/company_widget.dart';
+import 'package:space_x/injection_container.dart';
 
 class CompanyPage extends StatefulWidget {
   const CompanyPage({Key? key}) : super(key: key);
@@ -8,6 +14,8 @@ class CompanyPage extends StatefulWidget {
 }
 
 class _CompanyPageState extends State<CompanyPage> {
+  final bloc = sl<CompanyBloc>();
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -15,59 +23,39 @@ class _CompanyPageState extends State<CompanyPage> {
         gradient: LinearGradient(
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
-            colors: [Color(0xff102039), Color(0xff2A749B)]),
+            colors: kBackgroundGradient),
       ),
       child: Scaffold(
         backgroundColor: Colors.transparent,
-        appBar: AppBar(
-          title: Text("Company"),
-        ),
         drawer: Drawer(),
         body: buildBody(),
       ),
     );
   }
 
-  buildBody() {
-    return Container(
-      child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          children: [
-            Image.asset("assets/images/space_x_2.png"),
-            SizedBox(height: 20.0),
-            Text(
-              "SpaceX designs, manufactures and launches advanced rockets and spacecrafts. The company was founded in 2002 to revolutionize space technology, with the ultimate goal of enabling people to live on other"
-                  " planets.",
-              style: TextStyle(color: Colors.white),
-            ),
-            SizedBox(height: 30.0),
-            Align(
-              alignment: Alignment.topLeft,
-              child: Container(
-                width: 120.0,
-                child: Row(
-                  children: [
-                    Icon(Icons.location_on, color: Colors.white,),
-                    SizedBox(width: 10.0),
-                    Text("Location", style: TextStyle(color: Colors.white),),
-                  ],
-                ),
-              ),
-            ),
-            Column(
-              children: [
-                ListView(
-                  children: [
-                    Text("Location", style: TextStyle(color: Colors.white),), // NOT VISIBLE IN SCREEN
-                    Text("Location", style: TextStyle(color: Colors.white),), // NOT VISIBLE IN SCREEN
-                  ],
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
+  BlocProvider<CompanyBloc> buildBody() {
+    return BlocProvider(
+        create: (_) => bloc,
+      child: BlocBuilder<CompanyBloc, CompanyState>(
+          builder: (context, state) {
+            if (state is CompanyInitialState) {
+              _dispatchInit(context);
+              return CompanyLoadingView();
+            } else if (state is CompanyLoadingState) {
+              return CompanyLoadingView();
+            } else if (state is CompanyLoadedState) {
+              return CompanyBodyWidget();
+            } else if (state is CompanyErrorState) {
+              return ErrorWidget(state.errorMessage.toString());
+            }
+            return ErrorWidget("Unexpected error occurred");
+          }),
     );
   }
+
+  void _dispatchInit(context) {
+    BlocProvider.of<CompanyBloc>(context)..add(GetCompanyDataListEvent());
+  }
 }
+
+
