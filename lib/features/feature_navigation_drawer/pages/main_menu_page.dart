c30d0c4456ded04ba6fdabeebee684c07febeff5
sunalii <sunaliepic@gmail.com>
2021-09-17 12:55:56 +0530

    minor changes made in error handling

--- lib/features/feature_navigation_drawer/pages/main_menu_page.dart
+++ lib/features/feature_navigation_drawer/pages/main_menu_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:space_x/core/constants/colors.dart';
 import 'package:space_x/core/constants/constants.dart';
 import 'package:space_x/core/custom_icons/custom_icons_icons.dart';
+import 'package:space_x/features/feature_company/presentation/pages/company_page.dart';
 import 'package:space_x/features/feature_home/home_page.dart';
 import 'package:space_x/features/feature_missions/presentation/pages/missions_page.dart';
 import 'package:space_x/features/feature_navigation_drawer/widgets/drawer_body_widget.dart';
@@ -43,7 +44,7 @@ class _MainMenuState extends State<MainMenu> {
       mainTitle = DrawerNameList.nameList(DrawerSection.History);
     }
     else if(currentPage == DrawerSection.Company){
-      container = VehiclesPage();
+      container = CompanyPage();
       mainTitle = DrawerNameList.nameList(DrawerSection.Company);
     }
     else if(currentPage == DrawerSection.About){
