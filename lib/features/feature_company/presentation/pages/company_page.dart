8eda58019beb390b2950974b9e848c40b6140910
sunalii <sunaliepic@gmail.com>
2021-09-22 14:11:02 +0530

    Rockets loading view added as loading screens

--- lib/features/feature_company/presentation/pages/company_page.dart
+++ lib/features/feature_company/presentation/pages/company_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:space_x/core/constants/colors.dart';
 import 'package:space_x/features/feature_company/presentation/bloc/company_bloc.dart';
-import 'package:space_x/features/feature_company/presentation/widgets/company_loading.dart';
+import 'package:space_x/core/loadings/rockets_loading_view.dart';
 import 'package:space_x/features/feature_company/presentation/widgets/company_widget.dart';
 import 'package:space_x/injection_container.dart';
 
@@ -39,9 +39,9 @@ class _CompanyPageState extends State<CompanyPage> {
       child: BlocBuilder<CompanyBloc, CompanyState>(builder: (context, state) {
         if (state is CompanyInitialState) {
           _dispatchInit(context);
-          return CompanyLoadingView();
+          return RocketsLoadingView();
         } else if (state is CompanyLoadingState) {
-          return CompanyLoadingView();
+          return RocketsLoadingView();
         } else if (state is CompanyLoadedState) {
           print(state.companyList);
           return CompanyBodyWidget(company: state.companyList,);
