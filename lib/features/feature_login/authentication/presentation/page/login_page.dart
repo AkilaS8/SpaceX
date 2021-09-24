4d1e58e74ed4f5e7f4fa8f2cc1f8b3dee2a0ebda
yasmitha98 <51755151+yasmitha98@users.noreply.github.com>
2021-09-24 13:48:33 +0530

    firebase google ui

--- lib/features/feature_login/authentication/presentation/page/login_page.dart
+++ lib/features/feature_login/authentication/presentation/page/login_page.dart
@@ -1,24 +1,28 @@
-
 //import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:space_x/core/constants/colors.dart';
 import 'package:space_x/features/feature_home/home_page.dart';
 import 'package:space_x/features/feature_login/authentication/presentation/bloc/authentication_bloc.dart';
 import 'package:space_x/features/feature_navigation_drawer/pages/main_menu_page.dart';
+import 'package:auth_buttons/auth_buttons.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-
         body: Builder(
           builder: (context) {
             return BlocConsumer<AuthenticationBloc, AuthenticationState>(
               listener: (context, state) {
                 if (state is AuthenticationSuccess) {
-                  Navigator.pushReplacement(context,
-                      MaterialPageRoute(builder: (context) => MainMenu(userDeatils: state.authenticationDetail,)));
+                  Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => MainMenu(
+                                userDeatils: state.authenticationDetail,
+                              )));
                 } else if (state is AuthenticationFailiure) {
                   ScaffoldMessenger.of(context).showSnackBar(
                     SnackBar(
@@ -41,14 +45,20 @@ class LoginPage extends StatelessWidget {
                       mainAxisAlignment: MainAxisAlignment.center,
                       crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
-                        OutlinedButton(
+                        GoogleAuthButton(
                           onPressed: () =>
                               BlocProvider.of<AuthenticationBloc>(context).add(
-                                AuthenticationGoogleStarted(),
-                              ),
-                          child: Text('Login with Google'),
+                            AuthenticationGoogleStarted(),
+                          ),
                         ),
 
+                        // OutlinedButton(
+                        //   onPressed: () =>
+                        //       BlocProvider.of<AuthenticationBloc>(context).add(
+                        //         AuthenticationGoogleStarted(),
+                        //       ),
+                        //   child: Text('Login with Google'),
+                        // ),
                       ],
                     ),
                   );
